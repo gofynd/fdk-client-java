a20bef7d37af941d5a7ab153113b22c9773f96c7
Jigar Dafda <jigar.dafda@gmail.com>
2024-08-26 11:11:37 +0000

    [Auto Generated] 1.4.7-local

--- src/main/java/com/sdk/application/ApplicationHeaderInterceptor.java
+++ src/main/java/com/sdk/application/ApplicationHeaderInterceptor.java
@@ -39,6 +39,6 @@ public class ApplicationHeaderInterceptor implements Interceptor {
                 .addHeader("User-Agent", applicationConfig.getUserAgent())
                 .addHeader("Accept-Language", "en-IN")
                 .addHeader("Authorization", "Bearer "+bearerToken)
-                .addHeader("x-fp-sdk-version", "1.4.10-beta.1");
+                .addHeader("x-fp-sdk-version", "1.4.10-beta.2");
     }
 }
