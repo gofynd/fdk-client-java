ad9d4184a83a2dc27f8ad152ac86db03701c0e92
Jigar Dafda <jigar.dafda@gmail.com>
2025-02-12 11:55:59 +0000

    [Auto Generated] 3.0.0-beta.3

--- src/main/java/com/sdk/platform/PlatformHeaderInterceptor.java
+++ src/main/java/com/sdk/platform/PlatformHeaderInterceptor.java
@@ -37,7 +37,7 @@ public class PlatformHeaderInterceptor implements Interceptor {
                 .addHeader("Accept-Language", "en-IN")
                 .addHeader("Authorization", "Basic "+ bearerToken)
                 .addHeader("Content-Type","application/x-www-form-urlencoded")
-                .addHeader("x-fp-sdk-version", "3.0.0-beta.2");
+                .addHeader("x-fp-sdk-version", "3.0.0-beta.3");
     }            
 
 }
