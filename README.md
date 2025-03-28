b350f1a1e16c47225838c637cf9de2521fd91cb1
Jigar Dafda <jigar.dafda@gmail.com>
2025-03-28 14:41:59 +0000

    [Auto Generated] 0.0.1111-1.beta.2

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Get started with the Java Development SDK for Fynd Platform
 <dependency>
     <groupId>com.github.gofynd</groupId>
     <artifactId>fdk-client-java</artifactId>
-    <version>0.0.1111-1.beta.1</version>
+    <version>0.0.1111-1.beta.2</version>
 </dependency>
 ```
 
--- src/main/java/com/sdk/application/ApplicationHeaderInterceptor.java
+++ src/main/java/com/sdk/application/ApplicationHeaderInterceptor.java
@@ -39,6 +39,6 @@ public class ApplicationHeaderInterceptor implements Interceptor {
                 .addHeader("User-Agent", applicationConfig.getUserAgent())
                 .addHeader("Accept-Language", "en-IN")
                 .addHeader("Authorization", "Bearer "+bearerToken)
-                .addHeader("x-fp-sdk-version", "0.0.1111-1.beta.1");
+                .addHeader("x-fp-sdk-version", "0.0.1111-1.beta.2");
     }
 }
